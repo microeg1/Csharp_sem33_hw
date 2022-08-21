faae23eb98fedb0b3663c11ec7f17ccc8c6489eb
Egor <pro100egar@gmail.com>
2022-08-21 16:09:26 +0700

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int count = 1;
+        while (count <= n)
+        {
+            Console.Write(count + " ");
+            Console.WriteLine(Math.Pow(count,3));
+            count++;
+        }
+    }
+}
