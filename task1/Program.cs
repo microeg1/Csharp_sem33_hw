66e2fea3844001ed2b02ffa8303fac364dd52afd
Egor <pro100egar@gmail.com>
2022-08-21 16:09:02 +0700

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите пятизначное число:");
+        string number1 = Console.ReadLine();
+        if (number1[0] == number1[4])
+        {
+            if (number1[1] == number1[3])
+            {
+                Console.WriteLine(number1 + " - число палиндром.");
+            }
+            else
+            {
+                Console.WriteLine(number1 + " - не является палиндромом.");
+            }
+        }
+        else
+        {
+            Console.WriteLine(number1 + " - не является палиндромом.");
+        }
+
+    }
+}
